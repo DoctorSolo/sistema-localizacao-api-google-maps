799b785a536995b66dcb14c0dd3cc088aa5b47ba
Miguel Eduardo da Silva <miguelodoutor@gmail.com>
2026-08-21 11:03:58 -0300

    feat: switching object-oriented programming

--- src/GraphicInterface.py
+++ src/GraphicInterface.py
@@ -2,8 +2,8 @@ import customtkinter as ctk
 import theme_config
 
 from tkintermapview import TkinterMapView
-from PIL import Image
 from src.Local import Local
+from PIL import Image
 
 
 class GraphicInterface:
@@ -12,6 +12,7 @@ class GraphicInterface:
         ctk.set_default_color_theme(theme_config.CONFIG_THEME_BUTTON)
 
         self.__interface()
+    # END
     
     
     # This functions check if number
@@ -31,76 +32,101 @@ class GraphicInterface:
         window.title(theme_config.CONFIG_TITLE)
         window.geometry(theme_config.CONFIG_SIZE_CONFIG)
         
-        
         container_coordenates = ctk.CTkFrame(window)
-        container_coordenates.pack(padx=10, pady=10)
+        container_coordenates.pack(fill="both", expand=True, padx=10, pady=10)
         
         self.__create_empty_space(window, container_coordenates)
         
         window.mainloop()
     # END
     
+    # This function generate a title for a window
+    def __generate_title(self, frame: ctk.CTkFrame):
+        img = ctk.CTkImage(Image.open("assets/lupa.png"), size=(100, 100))
+        
+        title = ctk.CTkLabel(
+            frame,
+            image=None,
+            text="Enter the coordinate value below!",
+            pady=10,
+            compound="center",
+            font=("Arial", 30, "bold"),
+            text_color="#FFFFFF"
+        )
+        title.image = img
+        title.pack(padx=10, pady=10)
+    # END
+    
+    
     # Create a coordinate space
     def __create_empty_space(self, window: ctk.CTk, container: ctk.CTkFrame):
         validate = (window.register(self.__validate_number), '%P')
         
-        title = ctk.CTkLabel(
-            container,
-            text="Enter the coordinate value below.",
-            pady=10,
-            compound="center",            # Configure position
-            font=("Arial", 100, "bold"),# Configure font
-            text_color="#FFFFFF"      # Configure title color
-        )
-        title.pack(pady=10, padx=10)
+        self.__generate_title(container)
         
-        img = ctk.CTkImage(Image.open("assets/lupa.png"), size=(50, 50))
-        img_label = ctk.CTkLabel(
-            container,
-            image=img
-        )
-        img_label.image = img
-        img_label.pack(compound="right", padx=10, pady=10)
-        
-        description = ctk.CTkLabel(container, text="Put here the values of coordinates:")
-        description.pack(pady=10)
+        description_latitude = ctk.CTkLabel(container, text="Put here a latitude:")
+        description_latitude.pack(pady=10, padx=30)
         
         latitude = ctk.CTkEntry(
             container, validate="key", validatecommand=validate
         )
-        latitude.pack(pady=10, padx=10)
+        latitude.pack(pady=10, padx=30)
+        
+        description_longitude = ctk.CTkLabel(container, text="Put here a longitude:")
+        description_longitude.pack(pady=10, padx=30)
         
         longitude = ctk.CTkEntry(
             container, validate="key", validatecommand=validate
         )
-        longitude.pack(pady=10, padx=10)
+        longitude.pack(pady=10, padx=30)
         
-        output = ctk.CTkLabel(container, text="")
-        output.pack(padx=10, pady=10)
+        map = self.__generate_map(container)
+        out = self.__output(container)
         
-        map_widget = TkinterMapView(container, width=600, height=400, corner_radius=0)
+        enter_button = ctk.CTkButton(
+            container, text="ENTER", command=lambda: self.__inset_value(map, latitude, longitude, out)
+        )
+        enter_button.pack(padx=10, pady=10)
+    # END
+    
+    
+    def __output(self, frame: ctk.CTkFrame, text=""):
+        output = ctk.CTkLabel(frame, text=text)
+        output.grid(padx=10, pady=10)
+        return output
+    # END
+    
+    
+    def __generate_map(self, frame: ctk.CTkFrame, latitude=-14.2350, longitude=-51.9253):
+        map_widget = TkinterMapView(frame, width=600, height=400, corner_radius=0)
         map_widget.pack(padx=10, pady=10)
-
-        # Initial Position (ex: Brasil)
-        map_widget.set_position(-14.2350, -51.9253)
+        
+        map_widget.set_position(latitude, longitude)
         map_widget.set_zoom(4)
-
-        def __insert_value():
-            if not latitude.get() or not longitude.get():
-                output["text"] = "Fill both the field!"
-                return
-
-            lat = float(latitude.get())
-            long = float(longitude.get())
-            local0 = Local(lat, long)
-            local0.mapa()
-
-            # Atualiza o centro do mapa e adiciona um marcador
-            map_widget.set_position(lat, long)
-            map_widget.set_zoom(15)
-            map_widget.set_marker(lat, long, text="Local Encontrado")
-
-            output["text"] = local0.pesquisa()["Endereço"]
-
-        enter = ctk.CTkButton(container, text="ENTER", command=__insert_value)
-        enter.pack(pady=10)
+        return map_widget
+    # END
+    
+    
+    def __update_map(self, map_widget: TkinterMapView, latitude, longitude):
+        map_widget.set_position(latitude, longitude)
+        map_widget.set_zoom(15)
+        map_widget.set_marker(latitude, longitude, text="Local Found!")
+    # END
+    
+    
+    def __inset_value(self, map_widget: TkinterMapView, latitude, longitude, output):
+        if not latitude.get() or not longitude.get():
+            output["text"] = "Fill both the field!"
+            return
+        
+        lat = float(latitude.get())
+        long = float(longitude.get())
+        local = Local(lat, long)
+        local.mapa()
+        
+        self.__update_map(map_widget, latitude, longitude)
+        
+        output["text"] = local.pesquisa["Endereço"]
+    # END
+    
+    
